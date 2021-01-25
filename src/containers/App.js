7d873fd6c088ce71f5d9a4e700ddc14e01c32d9c
jonnisairio <jonni.sairio@netti.fi>
2021-01-26 01:18:55 +0200

    Modified App.js to ES6 and footer styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,54 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Footer from '../components/Footer';
 import './App.css';
-import { discs } from '../discs';
+import { discs as frisbeeDump } from '../discs';
 
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            discs: discs,
-            searchfield: ''
-        }
-    }
-
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    }
-
-    render() {
-        
-        // Filtering for the discs
-        const { discs, searchfield } = this.state;
-        const filterDiscs = discs.filter(disc =>  {
-
-            if (disc.name.toLowerCase().includes(searchfield.toLowerCase())) {
-                return disc.name;
-
-            } else if (disc.brand.toLowerCase().includes(searchfield.toLowerCase())) {
-                return disc.brand;
-
-            } else if (disc.flight.toLowerCase().includes(searchfield.toLowerCase())) {
-                return disc.flight;
-
-            } else if (disc.disctype.toLowerCase().includes(searchfield.toLowerCase())) {
-                return disc.disctype; 
-
-            }   return '';
-            
-        }) 
-        return (
-            <div className="app tc">
-            <h1 className="app">Jonni Sairio: In the bag</h1>
-            <SearchBox searchChange={this.onSearchChange} />
-            <CardList discs={filterDiscs} />
-            <Footer />
-            </div>
-        );
-    }
-}
-
-export default App;
+const App = () => {
+    const [filteredFrisbees, setFilteredFrisbees] = useState(frisbeeDump);
+    
+    const filterFrisbees = (event) => {
+    event.target.value === ""
+    ? setFilteredFrisbees(frisbeeDump)
+    : setFilteredFrisbees(
+    frisbeeDump.filter((frisbee) =>
+    [
+    frisbee.name.toLowerCase(),
+    frisbee.brand.toLowerCase(),
+    frisbee.flight.toLowerCase(),
+    frisbee.disctype.toLowerCase(),
+    ].some((text) => text.includes(event.target.value.toLowerCase()))
+    )
+    );
+    };
+    
+    return (
+    <div className="app tc">
+    <h1 className="app">Jonni Sairio: In the bag</h1>
+    <SearchBox searchChange={filterFrisbees} />
+    <CardList discs={filteredFrisbees} />
+    <Footer />
+    </div>
+    );
+    };
+    
+    export default App;
+    
